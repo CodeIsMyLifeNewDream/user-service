c637b1638dd13542334ef3a0eb50e7614e0f906a
sanjeebdas <sanjeeb155.das@gmail.com>
2026-04-02 17:08:33 +0700

    feat: Create a new REST endpoint version which will return version as “1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 a POC service called user service to test agentic SDLC 
+
+This service provides a REST API for managing users. The existing /api/users endpoint returns a list of users, and the new /api/version endpoint returns the version information as "1.0.0".
+
+To run the application:
+
+* Start the MySQL server
+* Run the command `mvn spring-boot:run` in the project root directory
+* Access the API at http://localhost:8080/api/users or http://localhost:8080/api/version
--- src/test/java/com/sage/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/sage/userservice/UserServiceApplicationTests.java
@@ -1,12 +1,20 @@
 package com.sage.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
-class UserServiceApplicationTests {
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserServiceApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
+    
+    // ... existing code ...
+    
     @Test
-    void contextLoads() {
+    public void testGetVersion() {
+        String version = webTestClient.get().uri("/api/version").exchange().expectStatus().isOk().returnResult(String.class).getResponseBody();
+        assertEquals("1.0.0", version);
     }
 }
